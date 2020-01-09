90073e1bba634fd356b4b0707814e4a27b2ff404
Denys <denys.cheporniuk@gmail.com>
2020-01-09 21:57:45 +0200

    Updates

---
+++ precache-manifest.f8dc6b0210212fcd24ca0b44f17e5fad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512045cc6e0ad5e2153656cfe9d864ac",
+    "url": "/TowerIQ_test-task/index.html"
+  },
+  {
+    "revision": "c85e1b324bcdc3327cfe",
+    "url": "/TowerIQ_test-task/static/css/main.c6d77491.chunk.css"
+  },
+  {
+    "revision": "57313d43a3fb365623f0",
+    "url": "/TowerIQ_test-task/static/js/2.0e37aec9.chunk.js"
+  },
+  {
+    "revision": "c85e1b324bcdc3327cfe",
+    "url": "/TowerIQ_test-task/static/js/main.47a03f10.chunk.js"
+  },
+  {
+    "revision": "965cb44d43351ac2fdb6",
+    "url": "/TowerIQ_test-task/static/js/runtime~main.d936c127.js"
+  }
+]);
